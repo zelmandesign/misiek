b1587d03ea6ec0c46a3a142668ead928b2825715
Andrzej <andrzej@zelmandesign.com>
2019-03-09 18:07:47 +0100

    nowe

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ client.on('guildMemberAdd', member => {
   // Do nothing if the channel wasn't found on this server
   if (!channel) return;
   // Send the message, mentioning the member
-  channel.send(`Welcome to the server, ${member}`);
+  channel.send(`Witaj na rejonie! Ej, ${member} pamietaj badz elegancki mordo`);
 });
 
 client.login(token);
